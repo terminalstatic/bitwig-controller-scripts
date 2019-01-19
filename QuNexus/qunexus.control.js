86b54ff25966fba69c9badd35a6332ad1b87dd1e
Wolfgang Grimm <grimm@mondial.at>
2019-01-19 17:00:57 +0100

    Simplified script, added multi channelversion

--- QuNexus/qunexus.control.js
+++ QuNexus/qunexus.control.js
@@ -9,18 +9,17 @@ host.defineController("Keith McMillen", "QuNexus", "0.1", "a535d5ee-82ac-429a-95
 
 host.defineMidiPorts(1, 1);
 
-host.addDeviceNameBasedDiscoveryPair(["QuNexus"], ["QuNexus"]);
+//host.addDeviceNameBasedDiscoveryPair(["QuNexus"], ["QuNexus"]);
 
 function init() {
-   transport = host.createTransport();
 
-   //noteIn = host.getMidiInPort(0).createNoteInput("QuNexus", "80????", "90????", "A0????", "B0????", "C0????", "D0????", "E0????");
-   noteIn = host.getMidiInPort(0).createNoteInput("QuNexus", "8?????", "9?????", "A?????", "B?????", "C?????", "D?????", "E?????");
+   println("Setting up omni only")
+   noteIn = host.getMidiInPort(0).createNoteInput("", "??????");
    noteIn.setShouldConsumeEvents(false);
 
    host.getMidiInPort(0).setMidiCallback(onMidi0);
    userControls = host.createUserControls(HIGHEST_CC - LOWEST_CC + 1);
-   for (var i = LOWEST_CC; i <= HIGHEST_CC; i++) {
+   for (let i = LOWEST_CC; i <= HIGHEST_CC; i++) {
       userControls.getControl(i - LOWEST_CC).setLabel("CC" + i);
    }
 
@@ -31,15 +30,13 @@ function onMidi0(status, data1, data2) {
    //printMidi(status, data1, data2)
    if (isChannelController(status)) {
       if (data1 >= LOWEST_CC && data1 <= HIGHEST_CC) {
-         var index = data1 - LOWEST_CC;
+         let index = data1 - LOWEST_CC;
          userControls.getControl(index).set(data2, 128);
       }
    }
 }
 
-function flush() {
-
-}
+function flush() {}
 
 function exit() {
 
---
+++ QuNexus/qunexusMulti.control.js
@@ -0,0 +1,48 @@
+loadAPI(7);
+
+const LOWEST_CC = 1;
+const HIGHEST_CC = 119;
+
+host.setShouldFailOnDeprecatedUse(true);
+
+host.defineController("Keith McMillen", "QuNexusMulti", "0.1", "a535d5ee-82ac-429a-9517-67f82048e02d", "terminal_static");
+
+host.defineMidiPorts(1, 1);
+
+//host.addDeviceNameBasedDiscoveryPair(["QuNexus"], ["QuNexus"]);
+
+function init() {
+
+    let noteIn = host.getMidiInPort(0).createNoteInput("All Channels", "??????");
+    noteIn.setShouldConsumeEvents(false);
+
+    for (let i = 0; i < 16; i++) {
+        let hex = i.toString(16).toUpperCase();
+        let noteIn = host.getMidiInPort(0).createNoteInput("Channel " + (i + 1), "?" + i.toString(16) + "????");
+        noteIn.setShouldConsumeEvents(false);
+    }
+
+    host.getMidiInPort(0).setMidiCallback(onMidi0);
+    userControls = host.createUserControls(HIGHEST_CC - LOWEST_CC + 1);
+    for (let i = LOWEST_CC; i <= HIGHEST_CC; i++) {
+        userControls.getControl(i - LOWEST_CC).setLabel("CC" + i);
+    }
+
+    println("qunexus initialized!");
+}
+
+function onMidi0(status, data1, data2) {
+    //printMidi(status, data1, data2)
+    if (isChannelController(status)) {
+        if (data1 >= LOWEST_CC && data1 <= HIGHEST_CC) {
+            let index = data1 - LOWEST_CC;
+            userControls.getControl(index).set(data2, 128);
+        }
+    }
+}
+
+function flush() {}
+
+function exit() {
+
+}
